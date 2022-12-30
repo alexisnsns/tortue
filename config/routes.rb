738f73ba9a83c1350e8add7a7d18ea679ad57af5
alexisns <alexisns@pm.me>
2022-12-30 16:19:13 +0100

    add teachings page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "", to: "pages#index"
   get "/life", to: "pages#life"
   get "/index", to: "pages#index"
+  get "/teachings", to: "pages#teachings"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
